d8539569bf01fdf5bd964d9d46dc3ccd9f088a82
katyashnaider <tnt_kote_97.05@mail.ru>
2022-10-02 16:19:24 +0300

    StepOne
    Добавлены RequireComponent на Animator и Rigidbody.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))] 
+[RequireComponent(typeof(Animator))] 
 
 public class Player : MonoBehaviour
 {
